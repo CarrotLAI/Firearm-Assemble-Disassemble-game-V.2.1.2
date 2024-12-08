82c26404bce040819b56959720489bf942f05219
LesterAi <lesterai001@gmail.com>
2024-12-08 22:47:34 +0800

    add buffering on videoplayer

--- Script/Tutorial/colt.gd
+++ Script/Tutorial/colt.gd
@@ -5,7 +5,19 @@ extends Control
 # var a = 2
 # var b = "text"
 var game_index = preload("res://Scene/UI/game_index.tscn")
+onready var video_player = $VideoPlayer
+
+func _ready():
+	yield(get_tree().create_timer(1.0), "timeout")
+	video_player.use_hw_decoder = true
+	video_player.stream = preload("res://Video/new_colt.webm")
+	video_player.play()
+
 
 func _on_Button_pressed():
 		get_tree().change_scene_to(game_index)
 #		yield(get_tree().create_timer(1), "timeout")  # Correct syntax for a delay
+
+
+func _on_VideoPlayer_finished():
+	video_player.play()
